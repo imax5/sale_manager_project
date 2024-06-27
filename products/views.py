7506e5d21268baa9e6651f22522bf6ab6e85a7c3
Maksat Chariev <maksatchariev@Maksats-MacBook-Pro.local>
2024-06-27 23:55:37 +0500

    initialed

--- products/views.py
+++ products/views.py
@@ -2,12 +2,9 @@ from django.views.generic import ListView
 
 from .models import Product
 
-class ContactList(ListView):
-    context_object_name = "contacts"
+class ProductList(ListView):
+    context_object_name = 'products'
+    model = Product
+    queryset = Product.objects.all()
     paginate_by = 50
-
-    def get_queryset(self):
-        user = self.request.user
-        if user.is_authenticated:
-            return Product.objects.filter(created_by=self.request.user)
-        return Product.objects.filter(created_by=None)
+    template_name = 'products_list.html'
